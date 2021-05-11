8f66a8400ce655bea81a17f71048c61b9ad27f88
ogaboss <goshensoftinc@gmail.com>
2021-05-11 11:51:10 +0100

    - Setup Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-pink-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
